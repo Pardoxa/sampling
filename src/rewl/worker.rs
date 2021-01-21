ad357e5bc3076b6c8fe986d743874e231ed9c659
Yannick Feld <yannick.feld@uol.de>
2021-01-21 17:24:55 +0100

    working on REWL

--- src/rewl/worker.rs
+++ src/rewl/worker.rs
@@ -1,10 +1,147 @@
 use std::sync::*;
 use rand::{Rng, SeedableRng};
+use std::num::NonZeroUsize;
+use crate::*;
 
 use rayon::prelude::*;
 
 #[cfg(feature = "serde_support")]
 use serde::{Serialize, Deserialize};
+
+
+fn merge_worker_prob<R, Hist>(worker: &mut [RewlWorker<R, Hist>])
+{
+    // The following if statement might be added later on - as of now it is unnessessary
+    //if worker.len() <= 2 {
+    //    return;
+    //}
+    let len = worker[0].probability_density.len();
+    debug_assert![worker.iter().all(|w| w.probability_density.len() == len)];
+
+    let num_workers_recip = (worker.len() as f64).recip();
+    for i in 0..len {
+        let mut val = worker[0].probability_density[i];
+        for w in worker[1..].iter()
+        {
+            val += w.probability_density[i];
+        }
+        val *= num_workers_recip;
+        for w in worker.iter_mut()
+        {
+            w.probability_density[i] = val;
+        }
+    }
+}
+
+#[derive(Debug, Clone)]
+#[cfg_attr(feature = "serde_support", derive(Serialize, Deserialize))]
+pub struct RewlWorker<R, Hist>
+{
+    id: usize,
+    sweep_size: NonZeroUsize,
+    rng: R,
+    hist: Hist,
+    probability_density: Vec<f64>,
+}
+
+impl<R, Hist> RewlWorker<R, Hist> 
+where R: Rng + Send + Sync,
+    Self: Send + Sync,
+    Hist: Histogram
+{
+    pub fn new(id: usize, rng: R, hist: Hist, sweep_size: NonZeroUsize) -> RewlWorker<R, Hist> {
+        let probability_density = vec![0.0; hist.bin_count()];
+        RewlWorker{
+            id,
+            rng,
+            hist,
+            probability_density,
+            sweep_size
+        }
+    }
+
+    pub fn do_work<Ensemble>(&mut self, ensemble_vec: &[RwLock<Ensemble>])
+    {
+        let mut e = ensemble_vec[self.id]
+            .write()
+            .expect("Fatal Error encountered; ERRORCODE 0x1 - this should be \
+                impossible to reach. If you are using the latest version of the \
+                'sampling' library, please contact the library author via github by opening an \
+                issue! https://github.com/Pardoxa/sampling/issues");
+        for _ in 0..self.sweep_size.get()
+        {
+
+        }
+    }
+}
+
+
+#[derive(Debug)]
+#[cfg_attr(feature = "serde_support", derive(Serialize, Deserialize))]
+pub struct Rewl<Ensemble, R, Hist>{
+    chunk_size: NonZeroUsize,
+    ensembles: Vec<RwLock<Ensemble>>,
+    map: Vec<usize>,
+    worker: Vec<RewlWorker<R, Hist>>
+}
+
+impl<Ensemble, R, Hist> Rewl<Ensemble, R, Hist> 
+where R: Send + Sync + Rng,
+    Hist: Send + Sync + Histogram
+{
+
+    pub fn new_from_other_rng<R2>(mut rng: &mut R2, ensembles: Vec<Ensemble>, hists: Vec<Hist>, chunk_size: NonZeroUsize, sweep_size: NonZeroUsize) -> Rewl<Ensemble, R, Hist>
+    where R2: Rng,
+        R: SeedableRng,
+    {
+        let map = (0..ensembles.len()).collect();
+        
+        let worker = (0..ensembles.len())
+            .zip(hists.into_iter())
+            .map(|(id, hist)| 
+                {
+                    let r = R::from_rng(&mut rng)
+                        .expect("Unable to seed Rng");
+                    RewlWorker::new(id, r, hist, sweep_size)
+                }
+            )
+            .collect();
+        
+        let e = ensembles.into_iter()
+            .map(|val| RwLock::new(val))
+            .collect();
+        Rewl{
+            map,
+            worker,
+            ensembles: e,
+            chunk_size
+        }
+    }
+
+    pub fn new(rng: &mut R, ensembles: Vec<Ensemble>, hists: Vec<Hist>, chunk_size: NonZeroUsize, sweep_size: NonZeroUsize) -> Rewl<Ensemble, R, Hist>
+    where R: Rng + SeedableRng
+    {
+       Self::new_from_other_rng(rng, ensembles, hists, chunk_size, sweep_size)
+    }
+
+    pub fn sweep_test(&mut self)
+    where Ensemble: TestEnsemble + Send + Sync,
+        R: Send + Sync
+    {
+        let slice = self.ensembles.as_slice();
+        self.worker
+            .par_iter_mut()
+            .for_each(|w| w.do_work(slice));
+
+
+        if self.chunk_size.get() >= 2 {
+            self.worker
+                .par_chunks_mut(self.chunk_size.get())
+                .for_each(merge_worker_prob);
+        }
+    }
+}
+
 
 pub trait TestEnsemble{
     fn num(&self) -> usize;
@@ -35,94 +172,6 @@ impl TestEnsemble for TestEns
     }
 }
 
-#[derive(Debug, Clone)]
-#[cfg_attr(feature = "serde_support", derive(Serialize, Deserialize))]
-pub struct RewlWorker<R>
-{
-    id: usize,
-    rng: R
-}
-
-impl<R> RewlWorker<R> 
-where R: Rng + Send + Sync
-{
-    pub fn new(id: usize, rng: R) -> RewlWorker<R> {
-        RewlWorker{
-            id,
-            rng
-        }
-    }
-
-    pub fn do_work<Ensemble>(&mut self, ensemble_vec: &[RwLock<Ensemble>])
-    where Ensemble: TestEnsemble
-    {
-        let mut e = ensemble_vec[self.id]
-            .write()
-            .expect("Fatal Error encountered; ERRORCODE 0x1 - this should be \
-                impossible to reach. If you are using the latest version of the \
-                'sampling' library, please contact the library author via github by opening an \
-                issue! https://github.com/Pardoxa/sampling/issues");
-        e.add_one();
-        print!("{} ", e.num())
-    }
-}
-
-
-#[derive(Debug)]
-#[cfg_attr(feature = "serde_support", derive(Serialize, Deserialize))]
-pub struct Rewl<Ensemble, R>{
-    ensembles: Vec<RwLock<Ensemble>>,
-    map: Vec<usize>,
-    worker: Vec<RewlWorker<R>>
-}
-
-impl<Ensemble, R> Rewl<Ensemble, R> 
-where R: Send + Sync + Rng
-{
-
-    pub fn new_from_other_rng<R2>(mut rng: &mut R2, ensembles: Vec<Ensemble>) -> Rewl<Ensemble, R>
-    where R2: Rng,
-        R: SeedableRng
-    {
-        let map = (0..ensembles.len()).collect();
-        
-        let worker = (0..ensembles.len())
-            .map(|id| 
-                {
-                    let r = R::from_rng(&mut rng)
-                        .expect("Unable to seed Rng");
-                    RewlWorker::new(id, r)
-                }
-            )
-            .collect();
-        
-        let e = ensembles.into_iter()
-            .map(|val| RwLock::new(val))
-            .collect();
-        Rewl{
-            map,
-            worker,
-            ensembles: e,
-        }
-    }
-
-    pub fn new(rng: &mut R, ensembles: Vec<Ensemble>) -> Rewl<Ensemble, R>
-    where R: Rng + SeedableRng
-    {
-       Self::new_from_other_rng(rng, ensembles)
-    }
-
-    pub fn sweep_test(&mut self)
-    where Ensemble: TestEnsemble + Send + Sync,
-        R: Send + Sync
-    {
-        let slice = self.ensembles.as_slice();
-        self.worker
-            .par_iter_mut()
-            .for_each(|w| w.do_work(slice));
-    }
-}
-
 #[cfg(test)]
 mod tests
 {
@@ -139,7 +188,7 @@ mod tests
         
         let mut rng = Pcg64Mcg::seed_from_u64(94375982592);
 
-        let mut rewl = Rewl::new(&mut rng, ensembles);
+        let mut rewl = Rewl::new(&mut rng, ensembles, 1);
 
         for _ in 0..10 {
             rewl.sweep_test();
