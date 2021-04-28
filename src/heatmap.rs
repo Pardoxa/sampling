566f5b78b340e91bb4273459f4ab8e717103ba88
Yannick Feld <yannick.feld@uol.de>
2021-04-28 12:20:56 +0200

    bugfix - no-default-features

--- src/heatmap.rs
+++ src/heatmap.rs
@@ -5,12 +5,16 @@ mod heatmap;
 mod helper;
 mod gnuplot;
 mod heatmap_float;
+#[cfg(feature="bootstrap")]
 mod heatmap_u_mean;
+#[cfg(feature="bootstrap")]
 mod heatmap_f_mean;
 
 pub use heatmap::*;
 pub use helper::*;
 pub use gnuplot::*;
 pub use heatmap_float::*;
+#[cfg(feature="bootstrap")]
 pub use heatmap_u_mean::*;
+#[cfg(feature="bootstrap")]
 pub use heatmap_f_mean::*;
